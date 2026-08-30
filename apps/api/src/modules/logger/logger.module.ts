06d57e60be792375d023486f747b5271282b6377
Oleshkooo <olehchoma@gmail.com>
2026-08-30 23:47:30 +0200

    Move the log message builders below the module
    Function declarations hoist, so the decorator that references them still
    resolves at class-definition time. Verified on a running app: the request
    line still prints.

--- apps/api/src/modules/logger/logger.module.ts
+++ apps/api/src/modules/logger/logger.module.ts
@@ -4,14 +4,6 @@ import type { IncomingMessage, ServerResponse } from 'node:http'
 import { env } from '../env/env.js'
 import { PINO_PRETTY_IGNORED_FIELDS, PINO_PRETTY_MESSAGE_FORMAT, PINO_REQUEST_LOG_IGNORED_PATHS } from './constants.js'
 
-function successMessage(request: IncomingMessage, response: ServerResponse, responseTime: number): string {
-    return `${request.method} ${request.url} ${response.statusCode} ${responseTime}ms`
-}
-
-function errorMessage(request: IncomingMessage, response: ServerResponse): string {
-    return `${request.method} ${request.url} ${response.statusCode}`
-}
-
 @Module({
     imports: [
         PinoLoggerModule.forRoot({
@@ -40,3 +32,11 @@ function errorMessage(request: IncomingMessage, response: ServerResponse): strin
     ],
 })
 export class LoggerModule {}
+
+function successMessage(request: IncomingMessage, response: ServerResponse, responseTime: number): string {
+    return `${request.method} ${request.url} ${response.statusCode} ${responseTime}ms`
+}
+
+function errorMessage(request: IncomingMessage, response: ServerResponse): string {
+    return `${request.method} ${request.url} ${response.statusCode}`
+}
